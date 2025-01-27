d78d891a091ea7886db0a74d07e46f96e368112b
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-01-27 01:32:49 -0500

    i

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function square(num){
-
+    return num * num;
 }
 
+console.log(square(3));
+
+const greet = (name) => `Hello, ${name}`;
+
+function createCounter(){
+    let count = 5;
+    return function (){
+        count++;
+        return count;
+    }
+}
